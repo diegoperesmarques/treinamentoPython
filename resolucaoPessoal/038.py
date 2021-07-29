902a76c7bb3f181b1958cb80ca93c3e1667209ed
diegoperesmarques <diegoperesmarques@gmail.com>
2021-07-29 11:52:43 -0300

    Resolução pessoal do desafio 38

---
+++ resolucaoPessoal/038.py
@@ -0,0 +1,19 @@
+'''
+escreva um programa que leia dois números
+inteiros e compare-os, mostrando na tela
+uma mensagem:
+
+O primeiro valor é maior
+O segundo valor é maior
+Não existe valor maior, os dois
+são iguais
+'''
+numero1 = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo número: '))
+
+if numero1 > numero2:
+    print('O primeiro valor é maior')
+elif numero1 == numero2:
+    print('Não existe valor maior')
+else:
+    print('O segundo número é maior')
