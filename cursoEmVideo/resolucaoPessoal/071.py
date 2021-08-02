f09124c28b5959d7f4bc14a5b3262cb6802ed408
diegoperesmarques <diegoperesmarques@gmail.com>
2021-08-02 20:49:09 -0300

    Curso em vídeo - Resolução pessoal 071

---
+++ cursoEmVideo/resolucaoPessoal/071.py
@@ -0,0 +1,14 @@
+'''
+crie um programa que simule o funcionamento de um caixa
+eletrônico. No início, pergunte ao usuário qual será o 
+valor a ser sacado (Número inteiro) e o programa vai 
+informar quantas cédulas de cada valor serão entregues.
+
+obs: considere que o caixa possui cédulas de 50, 20, 
+10 e 1 
+'''
+print('-'*30)
+print('Banco pessoal')
+print('-'*30)
+
+valor = int(input('Digite o valor para sacar: '))
