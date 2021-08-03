596563da4c7756d5652bc629fbf065c7e3f32f8c
diegoperesmarques <diegoperesmarques@gmail.com>
2021-08-02 21:59:20 -0300

    Curso em vídeo - Resolução Oficial - 070

---
+++ cursoEmVideo/resolucaoOficial/070.py
@@ -0,0 +1,30 @@
+'''
+crie um programa que leia o nome e o preço de vários produtos. O 
+programa deverá perguntar se o usuário vai continuar. No final, mostre:
+a) Qual é o total gasto na compra.
+b) Quantos produtos custam mais de R$ 1000;
+c) Qual é o nome do produto mais barato.
+'''
+total = totmil = menor = cont = 0
+barato = ''
+while True: 
+    produto = str(input('Nome do produto: '))
+    preco = float(input('Preco: R$ '))
+    cont += 1
+    total += preco
+
+    if preco > 1000:
+        totmil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format(' FIM DO PROGRAMA '))
+print(f'O total da compra foi R$ {total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {barato} que custa R$ {menor:.2f}')
