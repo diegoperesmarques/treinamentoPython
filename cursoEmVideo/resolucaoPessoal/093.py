f822f6b44b4ec76c0926f06e453e43dd17b1bfd6
computador <computador@estudo>
2021-08-12 11:11:52 -0300

    Curso em Vídeo - Resolução Pessoal 093 completa

--- cursoEmVideo/resolucaoPessoal/093.py
+++ cursoEmVideo/resolucaoPessoal/093.py
@@ -4,3 +4,34 @@ programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler 
 quantidade de gols feitos em cada partida. No final, tudo isso será guardado em 
 um dicionário, incluindo o total de gols feitos durante o campeonato.
 '''
+quantidadeGols = list()
+infoJogador = dict()
+somaGols = 0
+nome = str(input('Nome do Jogador: ')).strip()
+numeroPartidas = int(input(f'Quantas partidas {nome} jogou? '))
+
+for i in range(0, numeroPartidas):
+    quantidadeGols.append(int(input(f'Quantos gols na partida {i+1}? ')))
+    somaGols = somaGols + quantidadeGols[i]
+
+
+infoJogador = {'nome': nome, 'gols':quantidadeGols, 'total':somaGols}
+print('-=' * 30)
+print(infoJogador)
+print('-=' * 30)
+print('')
+
+for chave, valor in infoJogador.items():
+    print(f'O campo {chave} tem valor {valor}')
+
+print('')
+print('-=' * 30)
+
+print(f'O jogador {nome} jogou {numeroPartidas}')
+for posicao, valor in enumerate (quantidadeGols):
+    print(f'Na partida {posicao}, fez {valor} gols')
+
+
+print(f'Foi um total de {somaGols} gols')
+
+    
