773b9c3017970b67994101fdcf13db3b4c812b2c
diegoperesmarques <diegoperesmarques@gmail.com>
2021-08-17 10:50:59 -0300

    Curso em Vídeo - Aula 21 funções parte 2

---
+++ cursoEmVideo/aulas/aula21c.py
@@ -0,0 +1,14 @@
+def somar(a=0, b=0, c=0):
+    """
+    -> faz soma de três valores e mostra o resultado na tela.
+    :param a: o primeiro valor
+    :param b: o segundo valor
+    :param c: o terceiro valor
+    Função criada por Gustavo Guanabara do canal CursoemVideo
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+
+somar()
+somar(c=2, b=5)
