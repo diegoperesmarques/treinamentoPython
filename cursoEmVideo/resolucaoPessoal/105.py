7edf31def228a56fa5abdbdfa8ab7c8011dec236
diegoperesmarques <diegoperesmarques@hotmail.com>
2021-08-18 08:59:20 -0300

    Curso em Vídeo - Resolução Pessoal - 105 cabeçalho

---
+++ cursoEmVideo/resolucaoPessoal/105.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que tenha uma função notas() que pode receber várias notas
+de alunos e vai retornar um dicionários com as seguintes ifnormações:
+-Quantidade de notas
+-A maior notas
+-A manor nota 
+-A média da turma
+-A a situação (opcional)
+Adicione também as docstrings da função
+'''
