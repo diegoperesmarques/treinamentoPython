184470157a95d10c6d5ba01d13ed0f594896fa96
diegoperesmarques <diegoperesmarques@gmail.com>
2021-08-18 08:27:37 -0300

    Curso em Vídeo - Resolução Pessoa - 102

---
+++ cursoEmVideo/resolucaoPessoal/102.py
@@ -0,0 +1,28 @@
+'''
+Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro
+que indique o número a calcular e o outro chamado show, que será um valor lógico 
+(opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+'''
+def fatorial(numero, show=False):
+    """
+    -> Função para calcular o fatorial de um número digitado
+    :param numero: É o número que será calculado o fatorial
+    """
+    resultado = 1
+
+    if show == True:
+        for i in range(numero, 0, -1):
+            print(f' {i} x', end='')
+        print(' = ', end='')
+
+        for i in range(numero, 0, -1):
+            resultado *= i
+
+        print(resultado)
+    else:
+        for i in range(numero, 0, -1):
+            resultado *= i
+        print(resultado)
+
+#Programa principal
+fatorial(5)
