73c33011c09655875f8028210e63cfb7ad65e979
diegoperesmarques <diegoperesmarques@hotmail.com>
2021-08-26 14:59:58 -0300

    Udemy - Python do Zero ao Avançado - Resolução Pessoal - Primeiro Projeto

--- udemy/python3zeroaoavancado/testes/primeiroProjeto/jogoDaVelha.py
+++ udemy/python3zeroaoavancado/testes/primeiroProjeto/jogoDaVelha.py
@@ -3,8 +3,14 @@
 O quadro deve ser impresso sempre que um jogador faz uma jogada
 Você deve ser capaz de aceitar a entrada da posição do jogador e, em seguida, colocar um símbolo na placa
 '''
+#importações
+import os
+
+#variáveis
 posicaoJogada = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 jogo = [[' ',' ',' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
+#funções
 def linhas(texto):
     tamanho = len(texto) + 4
     print('')
@@ -21,6 +27,7 @@ def imprimiTabuleiroOriginal():
         print('')
     print('')
 
+
 def imprimirTabuleiroAtual():
     print('')
     for indice, posicao in enumerate(jogo):
@@ -28,6 +35,7 @@ def imprimirTabuleiroAtual():
             print(f'| {jogo[indice][i]} |', end='')
         print('')
     print('')
+
 
 def jogadas(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2):
     print('')
@@ -39,30 +47,39 @@ def jogadas(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2):
             decisaoJogador2 = 0
             posicaoDigitada = int(input(f'{nomeJogador1} digite a posição: '))
             if posicaoDigitada == 1:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[0][0] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 2:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[0][1] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 3:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[0][2] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 4:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[1][0] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 5:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[1][1] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 6:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[1][2] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 7:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[2][0] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 8:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[2][1] = escolhaJogador1
                 imprimirTabuleiroAtual()
             elif posicaoDigitada == 9:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 jogo[2][2] = escolhaJogador1
                 imprimirTabuleiroAtual()
 
@@ -72,30 +89,39 @@ def jogadas(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2):
                 decisaoJogador1 = 0
                 posicaoDigitada = int(input(f'{nomeJogador2} digite a posição: '))
                 if posicaoDigitada == 1:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[0][0] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 2:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[0][1] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 3:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[0][2] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 4:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[1][0] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 5:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[1][1] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 6:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[1][2] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 7:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[2][0] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 8:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[2][1] = escolhaJogador2
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 9:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[2][2] = escolhaJogador2
                     imprimirTabuleiroAtual()
 
@@ -104,30 +130,39 @@ def jogadas(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2):
                 decisaoJogador1 = 1
                 posicaoDigitada = int(input(f'{nomeJogador1} digite a posição: '))
                 if posicaoDigitada == 1:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[0][0] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 2:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[0][1] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 3:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[0][2] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 4:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[1][0] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 5:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[1][1] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 6:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[1][2] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 7:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[2][0] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 8:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[2][1] = escolhaJogador1
                     imprimirTabuleiroAtual()
                 elif posicaoDigitada == 9:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     jogo[2][2] = escolhaJogador1
                     imprimirTabuleiroAtual()
 
@@ -135,7 +170,45 @@ def jogadas(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2):
     print('')
 
 
+def resultado(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2):
+    if (jogo[0][0] == jogo[0][1] == jogo[0][2] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[1][0] == jogo[1][1] == jogo[1][2] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[2][0] == jogo[2][1] == jogo[2][2] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[0][2] == jogo[1][2] == jogo[2][2] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[0][1] == jogo[1][1] == jogo[2][1] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[0][0] == jogo[1][0] == jogo[2][0] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[0][0] == jogo[1][1] == jogo[2][2] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
+    elif (jogo[0][2] == jogo[1][1] == jogo[2][0] == escolhaJogador1):
+        print(f'{nomeJogador1} venceu a partida!')
 
+    elif (jogo[0][0] == jogo[0][1] == jogo[0][2] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[1][0] == jogo[1][1] == jogo[1][2] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[2][0] == jogo[2][1] == jogo[2][2] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[0][2] == jogo[1][2] == jogo[2][2] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[0][1] == jogo[1][1] == jogo[2][1] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[0][0] == jogo[1][0] == jogo[2][0] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[0][0] == jogo[1][1] == jogo[2][2] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    elif (jogo[0][2] == jogo[1][1] == jogo[2][0] == escolhaJogador2):
+        print(f'{nomeJogador2} venceu a partida!')
+    else:
+        print('Deu velha :(')        
+
+
+#Início do Programa
 linhas('JOGO DA VELHA')
 nomeJogador1 = input('Jogador 1, digite seu nome: ').upper().strip()
 nomeJogador2 = input('Jogador 2, digite seu nome: ').upper().strip()
@@ -145,6 +218,9 @@ if escolhaJogador1 == 'X':
     escolhaJogador2 = '0'
 else:
     escolhaJogador2 = 'X'
+
+#limpar tela
+os.system('cls' if os.name == 'nt' else 'clear')
 
 linhas('INFORMAÇÕES')
 print(f'O jogador 1 é: {nomeJogador1} e escolheu: {escolhaJogador1}')
@@ -160,3 +236,7 @@ imprimiTabuleiroOriginal()
 linhas('JOGANDO')
 
 jogadas(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2)
+
+
+linhas('RESULTADO')
+resultado(nomeJogador1, nomeJogador2, escolhaJogador1, escolhaJogador2)
